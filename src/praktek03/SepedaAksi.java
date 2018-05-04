c9f4f44bb9fd1ae77ec6e5ee4ea5fda4e99cea24
RahmatHidayat <r.hidayat.fti2016@gmail.com>
2018-05-04 23:56:14 +0800

    Menggunakan method get

--- src/praktek03/SepedaAksi.java
+++ src/praktek03/SepedaAksi.java
@@ -6,14 +6,24 @@ public class SepedaAksi {
         s.setMerk("Dragon");
         s.setWarna("Merah");
         s.setHarga(1000);
+        // Gak bisa s.setJenisMotor();
         
         s.cetakInfo();
         
-        System.out.println("Merknya \t ");
+        System.out.println("Merknya \t: ");
         System.out.println(s.getMerk());
-        System.out.println("Warnanya \t ");
+        System.out.println("Warnanya \t: ");
         System.out.println(s.getWarna());
-        System.out.println("Harganya \t ");
+        System.out.println("Harganya \t: ");
         System.out.println(s.getHarga());
+        
+        SepedaMotor sm = new SepedaMotor();
+        sm.setMerk("Hamaya");
+        sm.setWarna("Hitam");
+        sm.setHarga(10000);
+        sm.setJenisMotor("Listrik");
+        sm.cetakInfo();
+        System.out.println("Jenis \t:"+sm.getJenisMotor());
+        sm.throlttle();
     }
 }
